61e75dffa371fdcd7448ad7ec884d5f2b9165a94
sitao <sitaohost@gmail.com>
2024-09-08 00:52:26 +0800

    微调

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, config
 from .models import db
 from flask_jwt_extended import JWTManager
-from .config import Config  # 导入配置文件
+from config import Config # 导入配置文件
+# 如果写成import config, 那么app.config.from_object(config.Config)
+
 
 def create_app():
     app = Flask(__name__, static_folder="../static", static_url_path='')
-    app.config.from_object(Config)  # 使用配置文件中的配置
+    app.config.from_object(Config) # 使用配置文件中的配置
     # 初始化插件
     db.init_app(app)
     JWTManager(app)
@@ -15,17 +17,12 @@ def create_app():
     
     return app
 
-    # bp = Blueprint('main', __name__, static_folder="./static", static_url_path='')
-    # app.register_blueprint(bp) # 注册蓝图 bp。蓝图（Blueprint）是一种将应用程序的各个功能模块化的方法
-
 def register_blueprints(app):
     # 在这里导入并注册蓝图
     from .routes.public import public_bp
     from .routes.login import login_bp
     from .routes.admin import admin_bp
     from .routes.students import student_bp
-
-    
 
     # 注册各个蓝图
     # 使用 url_prefix 来指定蓝图的 URL 前缀
